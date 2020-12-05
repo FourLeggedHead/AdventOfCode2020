d6adbe623eb56902b00022056e3f9654ccedd112
Eric Lauret <eric.lauret@live.com>
2020-12-05 13:18:47 +0100

    Solved part I of Day Four

---
+++ DayFour/Model/Passport.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayFour.Model
+{
+     public class Passport
+     {
+        public string Byr { get; set; }
+        public string Iyr { get; set; }
+        public string Eyr { get; set; }
+        public string Hgt { get; set; }
+        public string Hcl { get; set; }
+        public string Ecl { get; set; }
+        public string Pid { get; set; }
+        public string Cid { get; set; }
+
+        public Passport(string passport)
+        {
+            var match = Regex.Match(passport, @"byr:(?<Value>\S+)");
+            if (match.Success) Byr = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"iyr:(?<Value>\S+)");
+            if (match.Success) Iyr = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"eyr:(?<Value>\S+)");
+            if (match.Success) Eyr = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"hgt:(?<Value>\S+)");
+            if (match.Success) Hgt = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"hcl:(?<Value>\S+)");
+            if (match.Success) Hcl = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"ecl:(?<Value>\S+)");
+            if (match.Success) Ecl = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"pid:(?<Value>\S+)");
+            if (match.Success) Pid = match.Groups["Value"].Value;
+
+            match = Regex.Match(passport, @"cid:(?<Value>\S+)");
+            if (match.Success) Cid = match.Groups["Value"].Value;
+        }
+
+        public bool IsValid()
+        {
+            return Byr != null && Iyr != null && Eyr != null &&
+                Hgt != null && Hcl != null && Ecl != null && Pid != null;
+        }
+     }
+}
---
+++ DayFour/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using DayFour.Model;
+using System.Linq;
+
+namespace DayFour  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Three");
+
+            var path = @"Resources/input.txt";
+
+            try
+            {
+                if (!File.Exists(path)) throw new FileNotFoundException();
+
+                var lines = File.ReadAllLines(path);
+
+                if (lines.Length == 0) throw new Exception($"File is empty.");
+
+                var Passports = new List<Passport>();
+                var passportRecord = String.Empty;
+
+                for (var i=0; i<lines.Length;i++)
+                {
+                    if (lines[i] != String.Empty) passportRecord += lines[i] + " ";
+
+                    if (lines[i] == String.Empty || i == lines.Length - 1)
+                    {
+                        Passports.Add(new Passport(passportRecord));
+                        passportRecord = String.Empty;
+                    }
+                }
+
+                Console.WriteLine($"The count of valid passports is {Passports.Count(p=>p.IsValid())}.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
