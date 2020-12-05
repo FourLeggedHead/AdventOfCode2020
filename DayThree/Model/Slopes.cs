84be4c4aafb4cd31d64149a83c2e1d132b098b4e
Eric Lauret <eric.lauret@live.com>
2020-12-05 10:29:31 +0100

    Solved part II of day three

---
+++ DayThree/Model/Slopes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayThree.Model
+{
+    public class Slopes
+    {
+        public int Right { get; set; }
+        public int Down { get; set; }
+        public int EncounteredTrees { get; set; }
+    }
+}
--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using DayThree.Model;
+using System.Linq;
 
 namespace DayThree
 {
@@ -20,6 +22,8 @@ namespace DayThree
 
                 if (lines.Length == 0) throw new Exception($"File is empty.");
 
+                // Populate the area
+
                 var area = new List<char[]>();
 
                 foreach (var line in lines)
@@ -27,16 +31,36 @@ namespace DayThree
                     area.Add(line.ToCharArray());
                 }
 
+                // Identify the width of the pattern
+
                 var patternWidth = area[0].Length;
 
-                var treeCount = 0;
+                // Initialize slopes
 
-                for (int i = 0; i < area.Count; i++)
+                var slopes = new List<Slopes>();
+
+                slopes.Add(new Slopes { Down = 1, Right = 1, EncounteredTrees = 0 });
+                slopes.Add(new Slopes { Down = 1, Right = 3, EncounteredTrees = 0 });
+                slopes.Add(new Slopes { Down = 1, Right = 5, EncounteredTrees = 0 });
+                slopes.Add(new Slopes { Down = 1, Right = 7, EncounteredTrees = 0 });
+                slopes.Add(new Slopes { Down = 2, Right = 1, EncounteredTrees = 0 });
+
+                // Go down the slopes
+
+                foreach (var slope in slopes)
                 {
-                    if (area[i][3 * i % patternWidth] == '#') treeCount++;
+                    for (int i = 0; i < area.Count; i++)
+                    {
+                        if (slope.Down * i < area.Count &&
+                            area[slope.Down * i][(slope.Right * i) % patternWidth] == '#') slope.EncounteredTrees++;
+                    }
+
+                    Console.WriteLine($"The number of trees encountered in the slope {slope.Right}-{slope.Down} is {slope.EncounteredTrees}.");
                 }
 
-                Console.WriteLine($"The number of trees encountered is {treeCount}.");
+                // Figure out the multiply
+
+                Console.WriteLine($"The multiply is {slopes.Aggregate<Slopes, long>(1, (multiply, slope) => multiply *= slope.EncounteredTrees)}.");
             }
             catch (Exception ex)
             {
