672b0ad15f59a19be10820eb610ec97ca8dacfc1
Eric Lauret <eric.lauret@live.com>
2020-12-06 09:36:20 +0100

    Solved part I of Day Six

---
+++ DaySix/Model/Group.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DaySix.Model
+{
+    public class Group
+    {
+        public List<char[]> PersonsAnswers { get; set; }
+
+        public Group()
+        {
+            PersonsAnswers = new List<char[]>();
+        }
+
+        public List<char> ListUniqueYesses()
+        {
+            var UniqueYesses = new List<char>();
+
+            foreach (var person in PersonsAnswers)
+            {
+                UniqueYesses = UniqueYesses.Union(person.ToList()).ToList();
+            }
+
+            return UniqueYesses;
+        }
+    }
+}
---
+++ DaySix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using DaySix.Model;
+
+namespace DaySix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Six");
+
+            var path = @"Resources/input.txt";
+
+            try
+            {
+                if (!File.Exists(path)) throw new FileNotFoundException();
+
+                var lines = File.ReadAllLines(path);
+
+                if (lines.Length == 0) throw new Exception($"File is empty.");
+
+                var PassengerGroups = new List<Group>();
+                var PassengerGroup = new Group();
+
+                for (var i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] != String.Empty) PassengerGroup.PersonsAnswers.Add(lines[i].ToCharArray());
+
+                    if (lines[i] == String.Empty || i == lines.Length - 1)
+                    {
+                        PassengerGroups.Add(PassengerGroup);
+                        PassengerGroup = new Group();
+                    }
+                }
+
+                Console.WriteLine($"The sum of the yesses counts is {PassengerGroups.Select(g => g.ListUniqueYesses().Count).Sum()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
