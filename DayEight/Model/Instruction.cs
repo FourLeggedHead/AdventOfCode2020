82db9b8e7bd0651eb2dc119fd03405415b7c3037
Eric Lauret <eric.lauret@live.com>
2020-12-08 08:31:04 +0100

    Solved part I of Day Eight

---
+++ DayEight/Model/Instruction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DayEight.Model
+{
+    public enum Operation
+    {
+        NoOperation,
+        Jump,
+        Accumulate
+    }
+
+    public class Instruction
+    {
+        public Operation Operation { get; set; }
+        public int Argument { get; set; }
+        public int RunCount { get; set; }
+
+        public Instruction(string instruction)
+        {
+            var instructionMatch = Regex.Match(instruction, @"(?<Operation>nop|acc|jmp) (?<Argument>[+-]\d+)");
+
+            if (instructionMatch.Success)
+            {
+                Operation = instructionMatch.Groups["Operation"].Value switch
+                {
+                    "jmp" => Operation.Jump,
+                    "acc" => Operation.Accumulate,
+                    _ => Operation.NoOperation,
+                };
+
+                Argument = int.Parse(instructionMatch.Groups["Argument"].Value);
+            }
+        }
+    }
+}
---
+++ DayEight/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FourLeggedHead.IO;
+using DayEight.Model;
+
+namespace DayEight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Eight");
+
+            var path = @"Resources/input.txt";
+
+            try
+            {
+                var instructions = FileReader.ReadAllLines(path);
+
+                var bootCode = new List<Instruction>();
+
+                foreach (var instruction in instructions)
+                {
+                    bootCode.Add(new Instruction(instruction));
+                }
+
+                var run = true;
+                var position = 0;
+                var accumulator = 0;
+
+                while (run)
+                {
+                    var instruction = bootCode[position];
+
+                    if (instruction.RunCount > 0) break; //run = false;
+
+                    switch (instruction.Operation)
+                    {
+                        case Operation.NoOperation:
+                            position++;
+                            break;
+                        case Operation.Jump:
+                            position += instruction.Argument;
+                            break;
+                        case Operation.Accumulate:
+                            position++;
+                            accumulator += instruction.Argument;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    instruction.RunCount++;
+                }
+
+                Console.WriteLine(accumulator);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
