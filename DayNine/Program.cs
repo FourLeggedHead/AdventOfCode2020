511bf48fdd85dfddc839723950e7a4392006fc55
Eric Lauret <eric.lauret@live.com>
2020-12-09 08:55:08 +0100

    Solved part I and II of Day Nine

---
+++ DayNine/Program.cs
@@ -0,0 +1,55 @@
+﻿using FourLeggedHead.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
+namespace DayNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Nine");
+
+            var path = @"Resources/input.txt";
+
+            try
+            {
+                var numbers = FileReader.ReadAllLines(path);
+
+                var XmasNumbers = new List<long>(numbers.Select(n => long.Parse(n)));
+
+                var preambleLength = 25;
+
+                for (int i = 25; i < XmasNumbers.Count; i++)
+                {
+                    if(!XmasNumbers.Skip(i - preambleLength).Take(25).Subsets(2).Any(s => s.Sum() == XmasNumbers[i]))
+                    {
+                        Console.WriteLine(XmasNumbers[i]);
+
+                        var windowSize = 2;
+
+                        while (windowSize < i)
+                        {
+                            var weakness = XmasNumbers.Take(i).Window(windowSize).FirstOrDefault(g => g.Sum() == XmasNumbers[i]);
+
+                            if (weakness != null)
+                            {
+                                Console.WriteLine(weakness.Min() + weakness.Max());
+
+                                break;
+                            }
+
+                            windowSize++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
