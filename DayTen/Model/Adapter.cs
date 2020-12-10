f09cc94ee5c77e3199578a84a2b05a3d7ec2e4f3
Eric Lauret <eric.lauret@live.com>
2020-12-10 08:23:03 +0100

    Solved part I of Day Ten

---
+++ DayTen/Model/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayTen.Model
+{
+    public class Adapter
+    {
+        public int Joltage { get; set; }
+        public bool Used { get; set; }
+
+        public Adapter(int joltage)
+        {
+            Joltage = joltage;
+            Used = false;
+        }
+
+        public override string ToString()
+        {
+            return Joltage + " : " + Used;
+        }
+    }
+}
---
+++ DayTen/Program.cs
@@ -0,0 +1,72 @@
+﻿using FourLeggedHead.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DayTen.Model;
+
+namespace DayTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Ten");
+
+            var path = @"Resources/input.txt";
+
+            try
+            {
+                var joltages = FileReader.ReadAllLines(path);
+
+                var Adapters = new List<Adapter>(joltages.Select(j => new Adapter(int.Parse(j))));
+
+                var deviceJoltage = Adapters.Max(a => a.Joltage) + 3;
+
+                var differences = new int[3] { 0, 0, 0 };
+                var joltage = 0;
+
+                while (joltage < deviceJoltage - 3)
+                {
+                    var adpater = Adapters.Find(a => a.Joltage == joltage + 1);
+                    if (adpater != null && !adpater.Used)
+                    {
+                        adpater.Used = true;
+                        joltage = adpater.Joltage;
+                        differences[0]++;
+                        continue;
+                    }
+
+                    adpater = Adapters.Find(a => a.Joltage == joltage + 2);
+                    if (adpater != null && !adpater.Used)
+                    {
+                        adpater.Used = true;
+                        joltage = adpater.Joltage;
+                        differences[1]++;
+                        continue;
+                    }
+
+                    adpater = Adapters.Find(a => a.Joltage == joltage + 3);
+                    if (adpater != null && !adpater.Used)
+                    {
+                        adpater.Used = true;
+                        joltage = adpater.Joltage;
+                        differences[2]++;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Houston we have a problem!");
+                    }
+                }
+
+                differences[2]++;
+
+                Console.WriteLine(differences[0] * differences[2]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
