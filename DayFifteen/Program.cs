64e7c681c51730fde4552908a7b26951c15c3837
Eric Lauret <eric.lauret@live.com>
2020-12-15 08:42:23 +0100

    Solved part II of Day Fifteen

--- DayFifteen/Program.cs
+++ DayFifteen/Program.cs
@@ -10,22 +10,32 @@ namespace DayFifteen
         {
             Console.WriteLine("Advent of Code 2020 - Day Fifteen");
 
-            //var numbers = new List<int>() { 17, 1, 3, 16, 19, 0 };
-            var numbers = new List<int>() { 0, 3, 6 };
+            var numbers = new List<long>() { 17, 1, 3, 16, 19, 0 };
 
-            while (numbers.Count < 2020)
+            var saidNumbers = new Dictionary<long, long>();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                saidNumbers.Add(numbers[i], i + 1);
+            }
+
+            var turn = numbers.Count;
+
+            while (turn < 30000000)
             {
                 var lastSpoken = numbers.Last();
 
-                if (numbers.SkipLast(1).Any(n => n == lastSpoken))
+                if (saidNumbers.ContainsKey(lastSpoken))
                 {
-                    var lastInstanceId = numbers.SkipLast(1).ToList().FindLastIndex(n => n == lastSpoken);
-                    numbers.Add(numbers.Count - lastInstanceId - 1);
+                    numbers.Add(turn - saidNumbers[lastSpoken]);
+                    saidNumbers[lastSpoken] = turn;
                 }
                 else
                 {
                     numbers.Add(0);
+                    saidNumbers.Add(lastSpoken, turn);
                 }
+
+                turn++;
             }
 
             Console.WriteLine(numbers.Last());
