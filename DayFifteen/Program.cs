9c491d4ece60badbe80f0155b623df503b1f399e
Eric Lauret <eric.lauret@live.com>
2020-12-15 07:28:31 +0100

    Solved part I of Day Fifteen

---
+++ DayFifteen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DayFifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Fifteen");
+
+            //var numbers = new List<int>() { 17, 1, 3, 16, 19, 0 };
+            var numbers = new List<int>() { 0, 3, 6 };
+
+            while (numbers.Count < 2020)
+            {
+                var lastSpoken = numbers.Last();
+
+                if (numbers.SkipLast(1).Any(n => n == lastSpoken))
+                {
+                    var lastInstanceId = numbers.SkipLast(1).ToList().FindLastIndex(n => n == lastSpoken);
+                    numbers.Add(numbers.Count - lastInstanceId - 1);
+                }
+                else
+                {
+                    numbers.Add(0);
+                }
+            }
+
+            Console.WriteLine(numbers.Last());
+        }
+    }
+}
