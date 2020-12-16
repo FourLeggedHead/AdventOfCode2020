8e78092b840102141a64147ef1be92f8a93a3f73
Eric Lauret <eric.lauret@live.com>
2020-12-16 07:42:25 +0100

    Solved part I of Day Sixteen

---
+++ DaySixteen/Model/Rule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DaySixteen.Model
+{
+    public class Rule
+    {
+        public string Name { get; set; }
+        public int FirstStart { get; set; }
+        public int FirstEnd { get; set; }
+        public int SecondStart { get; set; }
+        public int SecondEnd { get; set; }
+
+        public Rule(string rule)
+        {
+            var match = Regex.Match(rule, @"(?<Name>\D+\s*\D+): (?<FS>\d+)-(?<FE>\d+) or (?<SS>\d+)-(?<SE>\d+)");
+
+            if (match.Success)
+            {
+                Name = match.Groups["Name"].Value;
+                FirstStart = int.Parse(match.Groups["FS"].Value);
+                FirstEnd = int.Parse(match.Groups["FE"].Value);
+                SecondStart = int.Parse(match.Groups["SS"].Value);
+                SecondEnd = int.Parse(match.Groups["SE"].Value);
+            }
+        }
+
+        public bool Validate(int value)
+        {
+            return (value >= FirstStart && value <= FirstEnd)
+                || (value >= SecondStart && value <= SecondEnd);
+        }
+    }
+}
---
+++ DaySixteen/Program.cs
@@ -0,0 +1,66 @@
+﻿using FourLeggedHead.IO;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using DaySixteen.Model;
+
+namespace DaySixteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Sixteen");
+
+            try
+            {
+                var input = FileReader.ReadAllLines(@"Resources/input.txt").ToArray();
+
+                var i = 0;
+                var rules = new List<Rule>();
+
+                while (input[i] != "")
+                {
+                    rules.Add(new Rule(input[i]));
+                    i++;
+                }
+
+                var myTicket = new List<int>();
+                if (input[++i] == "your ticket:")
+                {
+                    myTicket.AddRange(input[++i].Split(',').Select(int.Parse));
+                    i++;
+                }
+
+                var nearbyTickets = new List<List<int>>();
+                if (input[++i] == "nearby tickets:")
+                {
+                    i++;
+                    while (i < input.Length)
+                    {
+                        nearbyTickets.Add(new List<int>(input[i].Split(',').Select(int.Parse)));
+                        i++;
+                    }
+                }
+
+                var scanningRate = 0;
+                foreach (var ticket in nearbyTickets)
+                {
+                    foreach (var number in ticket)
+                    {
+                        if (rules.Any(r => r.Validate(number)))
+                            continue;
+                        scanningRate += number;
+                    }
+                }
+
+                Console.WriteLine(scanningRate);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
