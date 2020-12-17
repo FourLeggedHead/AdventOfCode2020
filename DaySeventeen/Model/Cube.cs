8622e2e7ecbea56816d3397fe6debda5582c9f04
Eric Lauret <eric.lauret@live.com>
2020-12-17 09:17:46 +0100

    Prepared solution for part I of DaySeventeen

---
+++ DaySeventeen/Model/Cube.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DaySeventeen.Model
+{
+    public enum CubeState
+    {
+        Active,
+        Inactive
+    }
+
+    public class Cube
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public CubeState State { get; set; }
+
+        public Cube(int x, int y, int z, CubeState state)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            State = state;
+        }
+
+        public Cube(int x, int y, int z, char state)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+
+            switch (state)
+            {
+                case '.':
+                    State = CubeState.Inactive;
+                    break;
+                case '#':
+                    State = CubeState.Active;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return CustomHash(X, Y, Z);
+        }
+
+        int CustomHash(int x, int y, int z)
+        {
+            var seed = 10009;
+            var factor = 9176;
+            var hash = seed * factor + x.GetHashCode();
+            hash = hash * factor + y.GetHashCode();
+            hash = hash * factor + z.GetHashCode();
+            return hash;
+        }
+
+        public List<Cube> FindExistingNeighbors(Dictionary<int, Cube> space)
+        {
+            var neighbors = new List<Cube>();
+
+            for (int x = X - 1; x <= X + 1; x++)
+            {
+                for (int y = Y - 1; y <= Y + 1; x++)
+                {
+                    for (int z = Z - 1; z <= Z + 1; x++)
+                    {
+                        if (!(x == X && y == Y && z == Z))
+                        {
+                            if (space.TryGetValue(CustomHash(x, y, z), out Cube cube))
+                                neighbors.Add(cube);
+                        }
+                    }
+                }
+            }
+
+            return neighbors;
+        }
+
+        public Dictionary<int, Cube> FindMissingNeighbors(Dictionary<int, Cube> space)
+        {
+            var neighbors = new Dictionary<int, Cube>();
+
+            for (int x = X - 1; x <= X + 1; x++)
+            {
+                for (int y = Y - 1; y <= Y + 1; x++)
+                {
+                    for (int z = Z - 1; z <= Z + 1; x++)
+                    {
+                        if (!(x == X && y == Y && z == Z))
+                        {
+                            if (!space.TryGetValue(CustomHash(x, y, z), out Cube cube))
+                                neighbors.Add(CustomHash(x, y, z), new Cube(x, y, z, CubeState.Inactive));
+                        }
+                    }
+                }
+            }
+
+            return neighbors;
+        }
+
+        public void UpdateState(Dictionary<int, Cube> space)
+        {
+            var activeNeighborsCount = FindExistingNeighbors(space).Count(c => c.State == CubeState.Active);
+
+            if (State == CubeState.Active && activeNeighborsCount != 2 && activeNeighborsCount != 3)
+                State = CubeState.Inactive;
+
+            if (State == CubeState.Inactive && activeNeighborsCount == 3)
+                State = CubeState.Active;
+        }
+    }
+}
---
+++ DaySeventeen/Program.cs
@@ -0,0 +1,54 @@
+﻿using DaySeventeen.Model;
+using FourLeggedHead.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DaySeventeen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Seventeem");
+            try
+            {
+                var input = FileReader.ReadAllLines(@"Resources/input.txt");
+
+                var pocket = new Dictionary<int, Cube>();
+
+                for (int i = 0; i < input.Count(); i++)
+                {
+                    for (int j = 0; j < input.ElementAt(i).Length; j++)
+                    {
+                        var cube = new Cube(i, j, 0, input.ElementAt(i)[j]);
+                        pocket.Add(cube.GetHashCode(), cube);
+                    }
+                }
+
+                var cycles = 6;
+                for (int i = 0; i < cycles; i++)
+                {
+                    foreach (var cube in pocket)
+                    {
+                        var missingCubes = cube.Value.FindMissingNeighbors(pocket);
+                        foreach (var missingCube in missingCubes)
+                        {
+                            pocket.Add(missingCube.Key, missingCube.Value);
+                        }
+                    }
+
+                    var updatedPocket = new Dictionary<int, Cube>();
+                    foreach (var cube in pocket)
+                    {
+                        updatedPocket.Add(cube.Key, cube.Value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " - " + ex.StackTrace);
+            }
+        }
+    }
+}
