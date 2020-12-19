2df0aa5c714211dc1d5b20a7e5620cc6c8e4cf55
Eric Lauret <eric.lauret@live.com>
2020-12-19 13:18:48 +0100

    Solved part I of Day Nineteen

---
+++ DayNineteen/Model/Rule.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace DayNineteen.Model
+{
+    public class Rule
+    {
+        public int Id { get; set; }
+        public string Expression { get; set; }
+        public List<int> Children { get; set; }
+        public List<int> Parents{ get; set; }
+
+        public Rule(string rule)
+        {
+            var matchRule = Regex.Match(rule, @"(?<Id>\d+): (?<Expression>.+)");
+
+            if (!matchRule.Success)
+                throw new ArgumentException($"Input {rule} is not a valid rule.");
+
+            Id = int.Parse(matchRule.Groups["Id"].Value);
+            Expression = matchRule.Groups["Expression"].Value;
+
+            Children = ListUniqueChildren(Expression);
+        }
+
+        List<int> ListUniqueChildren(string expression)
+        {
+            if (expression.Contains('"')) return new List<int>();
+
+            return expression.Split(' ').ToList()
+                .Where(e => e != "|").Select(i => int.Parse(i))
+                .Distinct().OrderBy(i => i).ToList();
+        }
+
+        public void ListParents(Dictionary<int,Rule> rules)
+        {
+            Parents = rules
+                .Where(r => r.Value.Children.Count > 0 && r.Value.Children.Contains(Id))
+                .Select(r => r.Key).ToList();
+        }
+
+        public List<string> ListValidMessages(Dictionary<int, Rule> rules)
+        {
+            if (Expression.Contains('"')) return new List<string>() { Expression.Trim('"') };
+
+            var messages = new List<string>();
+
+            var options = Expression.Split('|').Select(o => o.Trim());
+
+            foreach (var option in options)
+            {
+                var opt = option.Split(" ").Select(i => int.Parse(i)).ToList();
+
+                if (opt.Count == 1)
+                {
+                    messages.AddRange(rules[opt[0]].ListValidMessages(rules));
+                }
+
+                if (opt.Count == 2)
+                {
+                    foreach (var left in rules[opt[0]].ListValidMessages(rules))
+                    {
+                        foreach (var right in rules[opt[1]].ListValidMessages(rules))
+                        {
+                            messages.Add(left + right);
+                        }
+                    }
+                }
+
+                if (opt.Count == 3)
+                {
+                    foreach (var left in rules[opt[0]].ListValidMessages(rules))
+                    {
+                        foreach (var middle in rules[opt[1]].ListValidMessages(rules))
+                        {
+                            foreach (var right in rules[opt[2]].ListValidMessages(rules))
+                            {
+                                messages.Add(left + middle + right);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return messages;
+        }
+    }
+}
---
+++ DayNineteen/Program.cs
@@ -0,0 +1,39 @@
+﻿using DayNineteen.Model;
+using FourLeggedHead.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace DayNineteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020 - Day Nineteen");
+
+            try
+            {
+                var input = FileReader.ReadAllLines(@"Resources/input.txt");
+
+                var rules = input.TakeWhile(i => i != "").Select(r => new Rule(r)).ToDictionary(r => r.Id);
+                foreach (var rule in rules)
+                {
+                    rule.Value.ListParents(rules);
+                }
+
+                var unparentedRules = rules.Where(r => r.Value.Parents.Count == 0).ToList();
+
+                var validMessages = unparentedRules[0].Value.ListValidMessages(rules);
+
+                var messages = input.Skip(rules.Count + 1).ToList();
+
+                Console.WriteLine(messages.Count(m => validMessages.Contains(m)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
