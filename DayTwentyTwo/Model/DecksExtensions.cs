f47fb3d5529478c0bd8d6746b5689ca586aca9b1
Eric Lauret <eric.lauret@live.com>
2020-12-22 13:43:07 +0100

    Solved part II of Day TwentyTwo

---
+++ DayTwentyTwo/Model/DecksExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace DayTwentyTwo.Model
+{
+    public static class DecksExtensions
+    {
+        public static void PrintDecks(this Dictionary<string, Queue<int>> decks)
+        {
+            var firstPlayer = decks.Keys.ElementAt(0);
+            var secondPlayer = decks.Keys.ElementAt(1);
+
+            Console.WriteLine(string.Join(',', decks[firstPlayer])
+                    + " | " + string.Join(',', decks[secondPlayer]));
+            Console.WriteLine();
+        }
+
+        public static int GetSequenceHashCode<T>(this IEnumerable<T> sequence)
+        {
+            const int seed = 487;
+            const int modifier = 31;
+
+            unchecked
+            {
+                return sequence.Aggregate(seed, (current, item) =>
+                    (current * modifier) + item.GetHashCode());
+            }
+        }
+    }
+}
--- DayTwentyTwo/Program.cs
+++ DayTwentyTwo/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
+using DayTwentyTwo.Model;
+using System.Numerics;
 
 namespace DayTwentyTwo
 {
@@ -14,7 +16,9 @@ namespace DayTwentyTwo
 
             try
             {
-                var input = FileReader.ReadAllLines(@"Resources/inputtest.txt").ToList();
+                var input = FileReader.ReadAllLines(@"Resources/input.txt").ToList();
+
+                // Part I
 
                 var decks = input
                     .Segment(l => l.StartsWith("Player"))
@@ -22,12 +26,30 @@ namespace DayTwentyTwo
                                     s => new Queue<int>(s.Where(l => !l.StartsWith("Player") && !string.IsNullOrWhiteSpace(l))
                                                         .Select(int.Parse)));
 
-                var firstPlayer = decks.Keys.ElementAt(0);
-                var secondPlayer = decks.Keys.ElementAt(1);
-
                 var winner = PlayingSimpleGame(decks);
 
+                decks.PrintDecks();
+
                 var score = 0;
+                while (decks[winner].Count > 0)
+                {
+                    score += decks[winner].Count * decks[winner].Dequeue();
+                }
+
+                Console.WriteLine(score);
+
+                // Part II
+
+                decks = input
+                    .Segment(l => l.StartsWith("Player"))
+                    .ToDictionary(s => s.ElementAt(0).Trim(':'),
+                                    s => new Queue<int>(s.Where(l => !l.StartsWith("Player") && !string.IsNullOrWhiteSpace(l))
+                                                        .Select(int.Parse)));
+                winner = PlayingRecursiveGame(decks);
+
+                decks.PrintDecks();
+
+                score = 0;
                 while (decks[winner].Count > 0)
                 {
                     score += decks[winner].Count * decks[winner].Dequeue();
@@ -42,7 +64,7 @@ namespace DayTwentyTwo
             }
         }
 
-        static string PlayingSimpleGame( Dictionary<string, Queue<int>> decks)
+        static string PlayingSimpleGame(Dictionary<string, Queue<int>> decks)
         {
             var firstPlayer = decks.Keys.ElementAt(0);
             var secondPlayer = decks.Keys.ElementAt(1);
@@ -61,6 +83,66 @@ namespace DayTwentyTwo
                 {
                     decks[secondPlayer].Enqueue(cardSecond);
                     decks[secondPlayer].Enqueue(cardFirst);
+                }
+            }
+
+            return decks.Where(d => d.Value.Count != 0).Select(d => d.Key).ElementAt(0);
+        }
+
+        static string PlayingRecursiveGame(Dictionary<string, Queue<int>> decks)
+        {
+            var firstPlayer = decks.Keys.ElementAt(0);
+            var secondPlayer = decks.Keys.ElementAt(1);
+
+            var history = new HashSet<(int, int)>();
+
+            while (decks.Values.All(d => d.Count > 0))
+            {
+                var decksIdentifier = (decks[firstPlayer].GetSequenceHashCode(), decks[secondPlayer].GetSequenceHashCode());
+
+                if (history.Contains(decksIdentifier))
+                {                    
+                    return firstPlayer;
+                }
+
+                history.Add(decksIdentifier);
+
+                var cardFirst = decks[firstPlayer].Dequeue();
+                var cardSecond = decks[secondPlayer].Dequeue();
+
+                if (decks[firstPlayer].Count < cardFirst || decks[secondPlayer].Count < cardSecond)
+                {
+                    if (cardFirst > cardSecond)
+                    {
+                        decks[firstPlayer].Enqueue(cardFirst);
+                        decks[firstPlayer].Enqueue(cardSecond);
+                    }
+                    else
+                    {
+                        decks[secondPlayer].Enqueue(cardSecond);
+                        decks[secondPlayer].Enqueue(cardFirst);
+                    }
+                }
+                else
+                {
+                    var subDecks = new Dictionary<string, Queue<int>>
+                    {
+                        { firstPlayer, new Queue<int>(decks[firstPlayer].Take(cardFirst)) },
+                        { secondPlayer, new Queue<int>(decks[secondPlayer].Take(cardSecond)) }
+                    };
+
+                    var winner = PlayingRecursiveGame(subDecks);
+
+                    if (winner == firstPlayer)
+                    {
+                        decks[firstPlayer].Enqueue(cardFirst);
+                        decks[firstPlayer].Enqueue(cardSecond);
+                    }
+                    else
+                    {
+                        decks[secondPlayer].Enqueue(cardSecond);
+                        decks[secondPlayer].Enqueue(cardFirst);
+                    }
                 }
             }
 
